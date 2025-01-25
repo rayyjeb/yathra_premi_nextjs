1997cc34ee0d1650fdc7077c8d4722f9285f56db
Rajab Shoukath Kunnath <142793649+rayyjeb@users.noreply.github.com>
2025-01-25 13:56:36 +0530

    Delete Work directory

--- Work/YathraPremi/yathrapremi_nextjs/yp_nxt/components/component/textanimate.tsx
+++
@@ -1,9 +0,0 @@
-import { TextAnimate } from "@/components/ui/text-animate";
-
-export function TextAnimateDemo() {
-  return (
-    <TextAnimate animation="blurInUp" by="character">
-        What our Guests Are Saying
-    </TextAnimate>
-  );
-}
--- Work/YathraPremi/yathrapremi_nextjs/yp_nxt/components/component/textreveal.tsx
+++
@@ -1,9 +0,0 @@
-import { TextReveal } from "@/components/ui/text-reveal";
-
-export function TextRevealDemo() {
-  return (
-    <div className="z-10 flex items-center justify-center rounded-lg bg-white dark:bg-black">
-      <TextReveal text="#yathrapremi" />
-    </div>
-  );
-}
